e6ede44be4f9ed111f44ed9165106159f07961a0
Cherenity <50301874+Cherenity@users.noreply.github.com>
2024-03-30 02:26:57 +0200

    Initial Bot template

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import requests
 import discord
+from discord.ext import commands
 from hiddenFiles.tFile import token
 
-print(token())
-print('test')
+
+def main():
+  print('--Chrysomallos discord bot is awake!--')
+
+  bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+  @bot.event
+  async def on_ready():
+    print("Bot ready!")
+  
+  @bot.command()
+  async def hello(ctx):
+    await ctx.send("Hello there!")
+
+
+  bot.run(token())
+
+
+if __name__=="__main__":
+  main()
