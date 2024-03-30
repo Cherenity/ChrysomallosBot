1b9a937e4b14fb6bc47830a3adcd331402b037c0
Cherenity <50301874+Cherenity@users.noreply.github.com>
2024-03-31 00:52:58 +0200

    a few small changes

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ def main():
 
   bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+  intents = discord.Intents.default()
+  intents.message_content = True  # Enable the message content intent
+
   @bot.event
   async def on_ready():
+    print(f"'We have logged in as {bot.user})")
     print("Bot ready!")
   
   @bot.command()
   async def hello(ctx):
-    await ctx.send("Hello there!")
+    await ctx.send(f"Hello there! {ctx.author.mention}!")
 
+  @bot.command(aliases=['gm','morning'])
+  async def goodmorning(ctx):
+    # Convert the user's input to lowercase
+    await ctx.send(f"Good Morning! {ctx.author.mention}!")
+  
 
   bot.run(token())
 
